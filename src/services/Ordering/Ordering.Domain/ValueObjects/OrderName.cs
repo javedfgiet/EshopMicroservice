ecac5086e87ca69a76ac604f09c01e1d565f2105
Adminuser <javedfgiet@gmail.com>
2024-10-24 16:07:28 +0000

    Completed Ordring.Domain Library

---
+++ src/services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.ValueObjects
+{
+    public record OrderName
+    {
+        public const int DefaultName = 5;
+        public string Value { get; set; }
+        private OrderName(string value)
+        {
+            Value = value;
+        }
+        public static OrderName Of(string value)
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(value);
+            ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DefaultName);
+            return new OrderName(value);
+        }
+
+    }
+}
